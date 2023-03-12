73539842636f8f512a78f54085cd388b0b4c57c3
Brett Haines <BHaines418@gmail.com>
2023-03-12 16:16:43 -0400

    Add name and portrait to dialogue UI

--- src/ui/conversation_dialog.gd
+++ src/ui/conversation_dialog.gd
@@ -3,6 +3,7 @@ extends Control
 
 
 # Display a given line, name, and image
-func display_line(text:String, _name:String, _imgpath:String):
-	$Panel/Label.text = text
-	# TODO: Add image and name label
+func display_line(text:String, name:String, imgpath:String):
+	$DialoguePanel/Label.text = text
+	$NamePanel/Label.text = name
+	$DialoguePanel/TextureRect.set_texture(load(imgpath))
